297a42fce0dbdf9214809914a46214c8a2b1e07d
Igor Botelho <igor_botelho_@outlook.com>
2021-04-10 21:28:10 -0300

    feat: Inclui estrutura testes com o mongo (#4)

---
+++ backend/test/teste-with-db.js
@@ -0,0 +1,37 @@
+const server = require("../src/db/server");
+
+async function testWithDb(fn) {
+  let mongoose;
+
+  beforeEach(async () => {
+    mongoose = await server();
+
+    const collectionsNames = await getCollections(mongoose);
+
+    await clearCollection(mongoose, collectionsNames);
+  });
+
+  afterAll(async () => {
+    mongoose.connection.close();
+  });
+
+  fn();
+}
+
+function getCollections(mongoose) {
+  return new Promise((resolve) => {
+    mongoose.connection.db.listCollections().toArray(function (err, names) {
+      return resolve(names);
+    });
+  });
+}
+
+async function clearCollection(mongoose, collectionsNames) {
+  if (collectionsNames) {
+    for ({ name } of collectionsNames) {
+      await mongoose.connection.collection(name).drop();
+    }
+  }
+}
+
+module.exports = testWithDb;
