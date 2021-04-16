4fcf871f5375c4c7a628a4a4cd2f6c6e9ab07c77
Igor Botelho <igor_botelho_@outlook.com>
2021-04-15 22:16:13 -0300

    feat(sevices): inclui serviço de conta (#33)

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Conta from "../services/conta/conta";
+
+function Index() {
+  return (
+    <div className="site-container">
+      <Conta />
+    </div>
+  );
+}
+
+export default Index;
