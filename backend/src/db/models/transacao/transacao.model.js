d7e753bf0101a61ad9667b2a0691f5d332da4534
Igor Botelho <igor_botelho_@outlook.com>
2021-04-15 21:47:55 -0300

    feat(services/transacao): inclui serviço de transação (#21)

--- backend/src/db/models/transacao/transacao.model.js
+++ backend/src/db/models/transacao/transacao.model.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const mongoose = require("mongoose");
 const schema = require("./transacao.schema");
 
@@ -6,5 +8,8 @@ const transacao = mongoose.model("Transacao", schema);
 module.exports = {
   cadastrar(dados) {
     return transacao.insertMany(dados);
+  },
+  consultar(query) {
+    return transacao.find(query);
   },
 };
--- backend/src/db/models/transacao/transacao.schema.js
+++ backend/src/db/models/transacao/transacao.schema.js
@@ -1,17 +1,23 @@
+"use strict";
+
 const mongoose = require("mongoose");
 
 module.exports = new mongoose.Schema(
   {
-    codigoBarras: { type: String },
     contaId: { type: mongoose.Schema.Types.ObjectId },
     dataHoraTransacao: { type: Date, required: true },
     linhaDigitavel: { type: String },
     tipo: {
       type: String,
-      enum: ["credito", "debito", "pagamento"],
+      enum: ["credito", "debito", "pagamento", "rendimento"],
       required: true,
     },
     valor: { type: Number, required: true, min: 0 },
+    status: {
+      type: String,
+      enum: ["pendente", "sucesso", "cancelado"],
+      required: true,
+    },
   },
   { versionKey: false }
 );
